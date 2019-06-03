a3e2da231eb02dfcf06f790fb397a968b31d72e2
hsrsbd <hsraffety@gmail.com>
2019-06-03 14:21:49 -0700

    updating styles, working on SearchBar.

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,17 +1,73 @@
 // react
 import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input
+} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PropTypes from 'prop-types';
 
 // styles
 import './SearchBar.scss';
 
 class SearchBar extends React.Component {
+  constructor() {
+    super();
+    {
+      this.state = {
+        isOpen: true
+      };
+    }
+  }
+
   render() {
     return (
       <div className='search-bar-container'>
-        <h1>Coming from SearchBar</h1>
+        <Navbar color='light' className='Navbar' light expand='md'>
+          <Nav className='ml-auto nav'>
+            <Input placeholder='Search' />
+            <NavItem>
+              <NavLink href='/components/'>FA Icon</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='https://github.com/reactstrap/reactstrap'>
+                FA Icon
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='https://github.com/reactstrap/reactstrap'>
+                FA Icon
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
       </div>
     );
   }
 }
 
 export default SearchBar;
+
+Navbar.propTypes = {
+  light: PropTypes.bool,
+  dark: PropTypes.bool,
+  fixed: PropTypes.string,
+  color: PropTypes.string,
+  role: PropTypes.string,
+  expand: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+  // pass in custom element to use
+};
