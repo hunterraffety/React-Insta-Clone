ca08baa942392b708a78f2e365be13fb43a39b87
hsrsbd <hsraffety@gmail.com>
2019-06-05 14:36:08 -0700

    building out if/else for auth.

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 // components
 import AuthComponent from './components/authentication/withAuthenticate';
-import PostsPage from './components/PostContainer/PostsPage';
-import LoginPage from './components/Login/LoginPage';
 
 // styles
 import 'bootstrap/dist/css/bootstrap.min.css';
