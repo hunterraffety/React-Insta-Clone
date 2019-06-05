4ff66763241b0a1da5029f50dff79ba01d0ba21a
hsrsbd <hsraffety@gmail.com>
2019-06-05 13:31:38 -0700

    updating components. styled horrible login.js

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -3,11 +3,12 @@ import React from 'react';
 // components
 import withAuthenticate from './components/authentication/withAuthenticate';
 import PostsPage from './components/PostContainer/PostsPage';
+import LoginPage from './components/Login/LoginPage';
 // styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
-const ComponentFromWithAuthenticate = withAuthenticate(PostsPage);
+const ComponentFromWithAuthenticate = withAuthenticate(LoginPage);
 
 class App extends React.Component {
   render() {
--- instagram-app/src/components/Login/LoginPage.js
+++ instagram-app/src/components/Login/LoginPage.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import Login from './Login';
 
-const LoginPage = () => {
-  return <Login />;
+// styles
+import './LoginPage.scss';
+
+class LoginPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      default: []
+    };
+  }
+  
+  return (
+    <div className='login-container'>
+      <div className='form-container'>
+        <form action='#' className='login-form' onSubmit={}>
+          <label htmlFor='Username'>Username</label>
+          <input type='text' />
+          <label htmlFor='Username'>Password</label>
+          <input type='password' />
+          <button>Log In</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
